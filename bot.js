3877f55c5d4181b980b7f8311fb7b21ba477e9c9
luca <luca@debian>
2017-02-27 21:00:53 +0100

    pre1

--- bot.js
+++ bot.js
@@ -1,3 +1,11 @@
+var cool = require('cool-ascii-faces')
+
+var asciiHeart = require("ascii-heart");
+
+const cows = require('cows');
+
+const vacca=require('vaca');
+
 var token = '309693128:AAH4TGkBXegyVLykKklS9P44emEgv56K9BM';
 
 var Bot = require('node-telegram-bot-api'),
@@ -6,7 +14,10 @@ var Bot = require('node-telegram-bot-api'),
 console.log('bot server started...');
 
 
-bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+
+
+
+bot.onText(/^\/somma((\s+\d+)+)$/, function (msg, match) {
   var result = 0;
   match[1].trim().split(/\s+/).forEach(function (i) {
     result += (+i || 0);
@@ -15,4 +26,40 @@ bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
     // reply sent!
   });
 });
+
+
+bot.onText(/faccia/,function (msg) {
+  bot.sendMessage(msg.chat.id, cool());
+});
+
+bot.onText(/mucca/,function (msg) {
+  var ii=Math.floor(Math.random() * 399) + 1;  
+  const opts = {
+    parse_mode: "HTML",
+  };
+  bot.sendMessage(msg.chat.id, "<code>"+ cows()[ii] + "</code>", opts);
+  console.log(cows()[ii]);
+});
+
+
+bot.onText(/cuore/,function (msg) {
+  const opts = {
+    parse_mode: "HTML",
+  };
+  bot.sendMessage(msg.chat.id, "<code>"+ asciiHeart(12,12) + "</code>", opts);
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 

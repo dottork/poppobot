bc4c8305cb9a9417947db56412bc2b4abe205cd3
luca <luca@debian>
2017-03-11 18:12:28 +0100

    com

--- bot.js
+++ bot.js
@@ -14,11 +14,11 @@ var Bot = require('node-telegram-bot-api');
 var bot;
 
 // if(process.env.NODE_ENV === 'production') {
-  bot = new Bot(token);
-  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+//  bot = new Bot(token);
+//  bot.setWebHook(process.env.HEROKU_URL + bot.token);
 // }
 // else {
-//   bot = new Bot(token, { polling: true });
+   bot = new Bot(token, { polling: true });
 // }
 
 

681e739ee3023fe5629749ebc01c65641073c8a6
luca <luca@debian>
2017-03-12 19:11:19 +0100

    corretto

--- web.js
+++ web.js
@@ -15,3 +15,7 @@ var server = app.listen(process.env.PORT, function () {
 });
 
 
+var http = require("http");
+setInterval(function() {
+    http.get("http://mighty-springs-60472.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
